95baf4d9cbf9c7bf010a743016d2365713af8c6c
MD. Mehrab Hossain <38918084+jansir17@users.noreply.github.com>
2022-06-09 13:49:25 +0600

    extending React.Component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM  from "react-dom";
 
-class Clock{
-  print(){
+class Clock extends React.Component{
+  render(){
     return(
   <h1 className="heading">
-  <span className="text">Hello {new Date().toLocaleTimeString()}</span>
+  <span className="text">Hello TIME:  {new Date().toLocaleTimeString()}</span>
   </h1>);
   }
 }
 
-const ClockComponent= new Clock();
-ReactDOM.render(ClockComponent.print(),document.getElementById('root'));
+ReactDOM.render(<Clock />,document.getElementById('root'));
