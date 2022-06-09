53c60f7b7c36b310a4068e5fda8cb390da2f5f73
MD. Mehrab Hossain <38918084+jansir17@users.noreply.github.com>
2022-06-09 20:40:51 +0600

    componentDidMount

--- src/component/Clock.js
+++ src/component/Clock.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 class Clock extends React.Component{
+  constructor(props){
+    super(props);
+    this.state ={date: new Date()};
+  }
+
+  componentDidMount(){
+    setInterval(()=>{
+      this.setState({
+        date: new Date(),
+      });
+    },1000)
+  }
+
   render(){
     return(
   <h1 className="heading">
-  <span className="text">Today's {this.props.children} {new Date().toLocaleTimeString(this.props.locale)}</span>
+  <span className="text">Today's {this.props.children} {this.state.date.toLocaleTimeString(this.props.locale)}</span>
   </h1>);
   }
 }
